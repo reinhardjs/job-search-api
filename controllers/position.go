b5e860bd155a43a9b38d0485f976c78c66d314de
reinhardjs <reinhardjsilalahi@gmail.com>
2022-11-26 19:37:39 +0700

    chore(job-search-api): refactor position controller

--- controllers/position.go
+++ controllers/position.go
@@ -16,6 +16,10 @@ var externalAPIEndpoint = "http://dev3.dansmultipro.co.id/api/recruitment"
 func GetPositions() http.HandlerFunc {
 	return func(rw http.ResponseWriter, r *http.Request) {
 		_, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		description := r.URL.Query().Get("description")
+		location := r.URL.Query().Get("location")
+		page := r.URL.Query().Get("page")
+
 		defer cancel()
 
 		rw.Header().Add("Content-Type", "application/json")
@@ -24,7 +28,7 @@ func GetPositions() http.HandlerFunc {
 		var client = &http.Client{}
 		var data []models.Position
 
-		request, err := http.NewRequest("GET", externalAPIEndpoint+"/positions.json", nil)
+		request, err := http.NewRequest("GET", externalAPIEndpoint+"/positions.json?description="+description+"&location="+location+"&page="+page, nil)
 		if err != nil {
 			response := responses.BaseResponse{Status: http.StatusOK, Message: err.Error(), Data: map[string]interface{}{}}
 			rw.WriteHeader(response.Status)
@@ -43,10 +47,7 @@ func GetPositions() http.HandlerFunc {
 
 		err = json.NewDecoder(res.Body).Decode(&data)
 		if err != nil {
-			response := responses.BaseResponse{Status: http.StatusOK, Message: err.Error(), Data: map[string]interface{}{}}
-			rw.WriteHeader(response.Status)
-			json.NewEncoder(rw).Encode(response)
-			return
+			data = []models.Position{}
 		}
 
 		response := responses.BaseResponse{Status: http.StatusOK, Message: "", Data: map[string]interface{}{"results": data}}
@@ -89,10 +90,7 @@ func GetPosition() http.HandlerFunc {
 
 		err = json.NewDecoder(res.Body).Decode(&data)
 		if err != nil {
-			response := responses.BaseResponse{Status: http.StatusOK, Message: err.Error(), Data: map[string]interface{}{}}
-			rw.WriteHeader(response.Status)
-			json.NewEncoder(rw).Encode(response)
-			return
+			data = models.Position{}
 		}
 
 		response := responses.BaseResponse{Status: http.StatusOK, Message: "", Data: data}
