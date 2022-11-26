1865e877ff6bb982323bdc751edf84c1168a5983
reinhardjs <reinhardjsilalahi@gmail.com>
2022-11-26 17:49:54 +0700

    chore(job-search-api): added db connection setup

---
+++ configs/setup.go
@@ -0,0 +1,48 @@
+package configs
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// Client instance
+var DB *mongo.Client = ConnectDB()
+
+func ConnectDB() *mongo.Client {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	mongoURI := os.Getenv("MONGODB_URI")
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//ping the database
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB")
+	return client
+}
+
+// getting database collections
+func GetCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	collection := client.Database("my-database").Collection(collectionName)
+	return collection
+}
